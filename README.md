0e625ae52f5c6b268bb8b5542b4fcbddc2863024
Paul <paul.almerino@smsupermalls.com>
2024-09-17 14:23:01 +0800

    PRDV-001 - Card Charging Setup

--- README.md
+++ README.md
@@ -9,3 +9,20 @@
 ## Payment Options
 
 - Xendit Credit Card
+
+## Sample Credit Card Charging
+
+```
+from smopay.card import Card
+
+card = Card()
+
+card.ref_id = "..."
+card.token_id = "..."
+card.authentication_id = "..."
+card.amount = "..."
+
+response = card.charge()
+
+print(response)
+```
--- smopay/card/__init__.py
+++ smopay/card/__init__.py
@@ -1,45 +1,40 @@
 import base64
+import os
 import requests
 
-XENDIT_SECRET_KEY = "xnd_development_Gr6cLvZPOJnjzTaO16BfN3oWfgoJMJxORAIo6ef3RMPqg2redZY37dIIBMdfWO"
+from smopay import Base
 
-authentication_id = "66e65ad786ee6a001a157189"
-token = "66e65ad786ee6a001a157188"
 
-def charge(opts):
-    ref_no = opts.get("ref_no")
-    token = opts.get("token")
-    authentication_id = opts.get("authentication_id")
-    amount = opts.get("amount")
+XENDIT_SECRET_KEY = os.getenv("XENDIT_SECRET_KEY")
+XP_SUB_ACCOUNT_MARKETPLACE = os.getenv("XP_SUB_ACCOUNT_MARKETPLACE")
 
-    auth_pair = f"{XENDIT_SECRET_KEY}:"
-    auth_base64 = base64.b64encode(auth_pair.encode())
-    auth_token = auth_base64.decode("utf-8")
 
-    header = {
-        "Authorization": f"Basic {auth_token}",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
+class Card(Base):
+
+  def __init__(self):
+    self.secret = XENDIT_SECRET_KEY
+    Base.__init__(self)
+
+
+  def charge(self):
+    auth_token = self._Base__authenticate()
 
     data = {
-        "external_id": ref_no,
-        "token_id": token,
-        "authentication_id": authentication_id,
-        "amount": amount,
+        "external_id": self.ref_id,
+        "token_id": self.token_id,
+        "authentication_id": self.authentication_id,
+        "amount": self.amount,
     }
 
-    return {
-        "gateway_reference_id": "TEST-0001",
-        "raw_json": {},
+    self.headers = {
+        "Authorization": f"Basic {auth_token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "for-user-id": XP_SUB_ACCOUNT_MARKETPLACE,
     }
 
     response = requests.post(
         "https://api.xendit.co/credit_card_charges",
-        headers=header,
+        headers=self.headers,
         data=data,
     )
-    print(f"response: {response.json()}")
-    return {
-        "gateway_reference_id": "TEST-0001",
-        "raw_json": response.json(),
-    }
+    return response.json()
